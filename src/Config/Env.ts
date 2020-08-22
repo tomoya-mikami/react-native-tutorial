3262d07f3ff67376077b747496453178be104f41
Tomoya Mikami <trymorecc@gmail.com>
2020-08-22 19:05:49 +0900

    expo config

---
+++ src/Config/Env.ts
@@ -0,0 +1,22 @@
+import Constants from "expo-constants";
+import Local from "./Local/Box";
+import Production from "./Production/Box";
+
+export interface EnvBox {
+  API_KEY: string;
+  API_URL: string;
+}
+
+export const getEnvBox = (): EnvBox => {
+  const env = Constants.manifest.releaseChannel;
+  if (env !== undefined) {
+    switch (env) {
+      case "prod":
+        return Production();
+      default:
+        return Local();
+    }
+  }
+
+  return Local();
+};
---
+++ src/Config/Local/Box.ts
@@ -0,0 +1,8 @@
+import { EnvBox } from "../Env";
+
+export default (): EnvBox => {
+  return {
+    API_KEY: "",
+    API_URL: "",
+  };
+};
